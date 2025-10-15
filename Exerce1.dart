44da31a009fdad1a753e97b38496ee8f95ba9bbe
taylor123marc <kenfacktaylor0@gmail.com>
2025-10-15 14:30:50 -0400

    question 3

--- Exerce1.dart
+++ Exerce1.dart
@@ -1,3 +1,19 @@
-void main () {
-  print ("hello taylor");
+class couleur {
+final int r, g, b;
+couleur(this.r, this.g, this.b);
+couleur.rouge() : r=255, g=0, b=0;
+couleur.vert() : r=0, g=255, b=0;
+couleur.bleu() : r=0, g=0, b=255;
+
+void affichercouleur()=>print("(R: $r, V:$g, B:$b)");
+}
+void main(){
+
+  couleur v=couleur.vert();
+  couleur r=couleur.rouge();
+  couleur b=couleur.bleu();
+
+  v.affichercouleur();
+  r.affichercouleur();
+  b.affichercouleur();
 }
