9240bb6535e946a0d21f1f0ce35826ed0d1d9504
taylor123marc <kenfacktaylor0@gmail.com>
2025-10-20 01:56:07 -0400

    question 8

--- exercice4.dart
+++ exercice4.dart
@@ -9,3 +9,27 @@ class Media {
     print("Ceci est un média générique.");
   }
 }
+
+class Livre extends Media {
+  final String auteur;
+
+  
+  Livre(String titre, this.auteur) : super(titre);
+
+    @override
+  void afficherType() {
+    print("Ceci est un Livre : $titre par $auteur.");
+  }
+}
+
+class Film extends Media {
+  final int dureeMinutes;
+
+  
+  Film(String titre, this.dureeMinutes) : super(titre);
+
+@override
+  void afficherType() {
+    print("Ceci est un Film : $titre (${dureeMinutes} min).");
+  }
+}
