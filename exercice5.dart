2225f0d837897ba35bad159ed6cae303ecbcbeb3
taylor123marc <kenfacktaylor0@gmail.com>
2025-10-20 02:17:45 -0400

    question 7

--- exercice5.dart
+++ exercice5.dart
@@ -17,3 +17,15 @@ class Cercle extends Forme {
     return 3.14 * rayon * rayon;
   }
 }
+
+class Rectangle extends Forme {
+  final double longueur;
+  final double largeur;
+
+  Rectangle(this.longueur, this.largeur);
+
+  @override
+  double calculerAire() {
+    return longueur * largeur;
+  }
+}
