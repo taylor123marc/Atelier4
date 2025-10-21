2c3c7c5c7316bf9d6d9c22052c785acfbc36fa3c
taylor123marc <kenfacktaylor0@gmail.com>
2025-10-21 01:06:03 -0400

    question 6

--- exercice6.dart
+++ exercice6.dart
@@ -2,3 +2,27 @@ abstract class Connectable {
   void connecter(String utilisateur);
   void deconnecter();
 }
+
+class ServeurAPI implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print("ServeurAPI : Connexion établie pour $utilisateur.");
+  }
+
+  @override
+  void deconnecter() {
+    print("ServeurAPI : Déconnexion réussie.");
+  }
+}
+
+class BaseDeDonnees implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print("BaseDeDonnees : Connexion etablie pour $utilisateur.");
+  }
+
+  @override
+  void deconnecter() {
+    print("BaseDeDonnees : Déconnexion réussie.");
+  }
+}
