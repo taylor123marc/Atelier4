f641211792a039f281d50b8403972866054ff88c
taylor123marc <kenfacktaylor0@gmail.com>
2025-10-27 17:37:38 -0400

    question 5 et fin de l'exo

--- exo7.dart
+++ exo7.dart
@@ -8,3 +8,16 @@ class Tache {
     Tache.nombreTotal++;
   }
    }
+
+
+void main() {
+  var a = Tache("part à l'ecole");
+  var b = Tache("etudie tes lecons");
+  var c = Tache("dort");
+
+  print(a.description);
+  print(b.description);
+  print(c.description);
+
+  print("Nombre de tâches créées : ${Tache.nombreTotal}");
+}   
